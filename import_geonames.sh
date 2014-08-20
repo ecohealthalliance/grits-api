8fddf8a3fae4c8a5307973305884fbea6c996a85
Nathan Breit <nabreit@gmail.com>
2014-08-20 04:16:44 +0000

    Moving geoname import code to annie

--- import_geonames.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget http://download.geonames.org/export/dump/allCountries.zip
-unzip allCountries.zip -d diagnosis
-rm allCountries.zip
-cd diagnosis && python mongo_import_geonames.py
-rm allCountries.txt
-cd ..
