a8b6c428303718e5b86947a2a185a2e32904237c
Russell Horton <rhorton@ecohealth.io>
2014-09-23 13:58:09 -0700

    don't send along datetime annotations that aren't DATEs with TimePoints https://github.com/ecohealthalliance/jvm-nlp/issues/2

--- diagnosis/Diagnoser.py
+++ diagnosis/Diagnoser.py
@@ -146,7 +146,8 @@ class Diagnoser():
             times_grouped = {}
             for span in anno_doc.tiers['times'].spans:
                 # TODO -- how should we handle DURATION and other exotice date types?
-                if span.type == 'DATE':
+                # Ignore them for now, only take DATEs that have a timePoint
+                if span.type == 'DATE' and 'timePoint' in span:
                     if not span.label in times_grouped:
                         times_grouped[span.label] = {
                             'type': 'datetime',
