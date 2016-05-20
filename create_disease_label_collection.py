abeb20ed39396849685f38bcb09ddd0194965441
Freddie Rosario <rosario@ecohealthalliance.org>
2016-05-20 09:52:51 -0400

    Add some debugging

--- create_disease_label_collection.py
+++ create_disease_label_collection.py
@@ -4,6 +4,12 @@ import config
 
 if __name__ == '__main__':
     db = MongoClient('%s/girder' % config.mongo_url)
+
+    type(db)
+    dir(db)
+    print '%s/girder' % config.mongo_url
+
+
     db.drop_collection('diseaseNames')
     for label in disease_label_table.get_labels():
         db['diseaseNames'].insert({'_id': label})
