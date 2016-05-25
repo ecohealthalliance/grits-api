4d57f4b2fe967c614835d8e57df2167fcbcae4a9
Freddie Rosario <rosario@ecohealthalliance.org>
2016-05-25 11:27:38 -0400

    Refactored config.py to allow for MONGO_URL env var

--- diagnosis/config.py
+++ diagnosis/config.py
@@ -1,1 +1,13 @@
-mongo_url = 'localhost'
+#This was the original entry
+#mongo_url = 'localhost'
+
+import os
+from urlparse import urlparse
+
+if 'MONGO_URL' in os.environ:
+  host = urlparse(os.environ['MONGO_URL']).hostname
+  mongo_url = host
+else:
+  mongo_url = 'localhost'
+
+
